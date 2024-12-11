ae250341f74086805051e2abe271f13a888609bd
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2024-12-11 13:11:46 +0100

    change /uploads to /tmp

--- src/multer.config.ts
+++ src/multer.config.ts
@@ -3,7 +3,7 @@ import { extname } from 'path';
 
 export const multerConfig = {
   storage: diskStorage({
-    destination: './uploads', // Папка для хранения файлов
+    destination: './tmp', // Папка для хранения файлов
     filename: (req, file, callback) => {
       const uniqueSuffix = Date.now() + extname(file.originalname);
       callback(null, file.fieldname + '-' + uniqueSuffix); // Уникальное имя файла
